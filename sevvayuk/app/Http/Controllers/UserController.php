56160b69559100aad6eea76741fb63520edb8550
Nurma dev <55167849+Nurma-tech@users.noreply.github.com>
2020-05-18 15:44:18 +0700

    Update UserController.php

--- sevvayuk/app/Http/Controllers/UserController.php
+++ sevvayuk/app/Http/Controllers/UserController.php
@@ -25,11 +25,32 @@ class UserController extends Controller
      */
     public function login()
     {
-        //
+        return view('user/login');
     }
     public function loginPost()
     {
-        //
+         $email = $request->string('email')->unique();
+        $password = $request->string('password');
+
+        $data = User::where('email',$email)->first();
+        if($data){ //apakah email tersebut ada atau tidak
+            //echo"hi";
+            $data = User::where('password',$password)->first();
+            if($data){
+                Session::put('email',$data->email);
+                Session::put('password',$data->password);
+                Session::put('username',$data->username);
+                Session::put('login',TRUE);
+            return redirect('user/home');
+            }
+            else{
+                return redirect('user/login')->with('alert','Check your username or password!');
+            }
+        }
+        else{
+            return redirect('user/login')->with('alert','Sorry Your password or email are not valid');
+        }
+    }
     }
     /**
      * Store a newly created resource in storage.
